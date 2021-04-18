a71eb64751e0843f77a90ab661662e846c886258
solrz <kbpsolrz@gmail.com>
2021-04-18 23:38:53 +0800

    Update to svite 2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,24 @@
-module.exports = {
-  optimizeDeps: {
-    exclude: ['@roxi/routify'],
-  },
-};
+
+// vite.config.js
+const svelte = require('@sveltejs/vite-plugin-svelte')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig(({ command, mode }) => {
+    const isProduction = mode === 'production'
+    return {
+        optimizeDeps: {
+            exclude: [
+                '@roxi/routify',
+            ],
+        },
+        plugins: [
+            svelte({
+                hot: !isProduction,
+                emitCss: true
+            })
+        ],
+        build: {
+            minify: isProduction
+        }
+    }
+})
